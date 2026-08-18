ae817610fe21d6ce795bd05b3dd02d9df1127507
Ralph <njugunaralph@gmail.com>
2026-08-19 02:06:11 +0300

    impliment full CRUD functionality for toys lab

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onUpdateToy, onRemoveToy }) {
+  const { id, name, image, likes } = toy;
+
+  function handleLikeClick() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ likes: likes + 1 }),
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => onUpdateToy(updatedToy));
+  }
+
+  function handleDonateClick() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    }).then(() => onRemoveToy(id));
+  }
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
-      <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
-        className="toy-avatar"
-      />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <h2>{name}</h2>
+      <img src={image} alt={name} className="toy-avatar" />
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLikeClick}>
+        Like {"<3"}
+      </button>
+      <button className="del-btn" onClick={handleDonateClick}>
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
